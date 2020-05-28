d1238a1504dbeb02acbe28afda7bacc99715e1e1
David Rodríguez <dalejorod@gmail.com>
2020-05-28 20:30:46 -0300

    actualizar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+import { InputGroup, FormControl, ListGroup } from "react-bootstrap";
 
-export default function App() {
+function App() {
+  const listaDeTareasInicial = ["Primera"];
+  const [tareas] = useState(listaDeTareasInicial);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InputGroup className="mb-3">
+        <FormControl placeholder="Nueva tarea" />
+      </InputGroup>
+      <ListGroup>
+        {tareas.map(tarea => (
+          <ListGroup.Item>{tarea}</ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }
+
+export default App;
